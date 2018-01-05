e97f9e5dc6faa136891cef19c1402fbe7e602a23
cloudtian <624714349@qq.com>
2018-01-06 00:14:05 +0800

    [update]代码eslint问题修改

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
 
@@ -14,4 +14,4 @@ new Vue({
   el: '#app',
   router,
   render: h => h(App)
-})
+});
